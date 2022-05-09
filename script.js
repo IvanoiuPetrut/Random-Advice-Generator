abd408a9e26cebaedd2b2851d6d0bd9ca1de2b11
Petrut <ivanoiu.petrut27@yahoo.com>
2022-05-09 15:19:44 +0300

    made the button unavailable for 3s after clicking it

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ const adviceNumber = document.querySelector("#advice-number");
 
 adviceBtn.addEventListener("click", function () {
   getAdvice();
+  makeUnavabile(adviceBtn);
 });
 
 window.onload = () => {
   getAdvice();
 };
 
+const adviceUrl = "https://api.adviceslip.com/advice";
+const controller = new AbortController();
+const timeoutId = setTimeout(() => controller.abort(), 2500);
+
 function getAdvice() {
-  fetch("https://api.adviceslip.com/advice")
+  fetch(adviceUrl, {
+    signal: controller.signal,
+  })
     .then((response) => {
       return response.json();
     })
@@ -23,4 +30,19 @@ function getAdvice() {
     .catch((error) => {
       console.log(error);
     });
+}
+
+function makeUnavabile(element) {
+  element.style.backgroundColor = "hsl(217, 19%, 38%)";
+  element.style.cursor = "not-allowed";
+  element.style.pointerEvents = "none";
+  setTimeout(function () {
+    makeAvailable(element);
+  }, 3000);
+}
+
+function makeAvailable(element) {
+  element.style.backgroundColor = "hsl(150, 100%, 66%)";
+  element.style.cursor = "pointer";
+  element.style.pointerEvents = "auto";
 }
