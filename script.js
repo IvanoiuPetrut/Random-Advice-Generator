dde8fd2929dbbf632f577edf6dcf447b42ecb15b
Petrut <ivanoiu.petrut27@yahoo.com>
2022-05-09 13:57:13 +0300

    added simple fetch from api and paragraph change

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ var h1 = document.querySelector("h1");
 let ct = 100;
 
 btn.addEventListener("click", function () {
-  p.textContent = ct;
-  ct += 100;
+  getAdvice();
 });
+
+function getAdvice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const adviceObj = data.slip;
+      p.textContent = adviceObj.advice;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
